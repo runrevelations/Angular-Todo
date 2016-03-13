ff1feee5039887857a04ed56822b60ad63e7c86a
Chris Almeida <run.revelations@gmail.com>
2016-03-12 19:23:33 -0800

    First Commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name animalsAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the animalsAppApp
+ */
+angular.module('animalsAppApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
